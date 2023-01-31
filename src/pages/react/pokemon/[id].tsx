fdce6aa74bd6b82877fbd75f27f19f15f79c6bb0
jaksonlima <jaksonsneider@gmail.com>
2023-01-30 23:34:41 -0300

    card

--- src/pages/react/pokemon/[id].tsx
+++ src/pages/react/pokemon/[id].tsx
@@ -6,14 +6,7 @@ import { Pokemon } from "@/types/Pokemon";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { PropsWithChildren, useEffect, useState } from "react";
-
-function SpecificPokemonImage({ children }: PropsWithChildren) {
-  return (
-    <div style={{ position: "absolute", top: "207px", left: "156px" }}>
-      {children}
-    </div>
-  );
-}
+import css from "@/styles/Pokemon.module.css";
 
 export default function SpecificPokemon() {
   const [pokemon, setPokemon] = useState<Pokemon>();
@@ -35,6 +28,9 @@ export default function SpecificPokemon() {
   }, [query]);
 
   const typeNames = pokemon?.types.map(({ type }) => type.name);
+  const linearGradient = `linear-gradient(to right, #403a3e, ${findColorOrRandom(
+    typeNames?.[0] || ""
+  )})`;
 
   return (
     <main
@@ -43,9 +39,7 @@ export default function SpecificPokemon() {
         justifyContent: "center",
         alignItems: "center",
         minHeight: "100vh",
-        background: `linear-gradient(to right, #403a3e, ${findColorOrRandom(
-          typeNames?.[0] || ""
-        )})`,
+        background: linearGradient,
       }}
     >
       <div style={{ position: "relative" }}>
@@ -64,7 +58,56 @@ export default function SpecificPokemon() {
             <Spinner />
           )}
         </div>
+        <div
+          style={{
+            position: "absolute",
+            top: "364px",
+            left: "93px",
+            display: "inline-flex",
+            gap: "5px",
+            alignItems: "center",
+          }}
+        >
+          <p>#{pokemon?.id}</p>
+          <h5>{pokemon?.name}</h5>
+        </div>
         <Image width={425} height={637} src="/pokedex.png" alt="pokedex" />;
+        <div
+          style={{
+            position: "absolute",
+            display: "flex",
+            flexWrap: "wrap",
+            gap: "10px",
+            bottom: "41px",
+            left: "32px",
+            background: linearGradient,
+            padding: "9px",
+            borderRadius: "6px",
+            maxWidth: "315px",
+            opacity: 0.7,
+          }}
+        >
+          {pokemon?.stats.map(({ stat, base_stat }) => (
+            <div style={{ display: "inline-flex", gap: "10px" }}>
+              <p
+                style={{
+                  color: "white",
+                  fontSize: "17px",
+                  fontWeight: "bold",
+                  textTransform: "capitalize",
+                }}
+              >
+                {stat.name}
+              </p>
+              <progress
+                id="stats"
+                value={base_stat}
+                max="100"
+                // style={{ background: linearGradient }}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </main>
   );
